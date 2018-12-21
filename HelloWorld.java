ede5cef115ab3f5c01d316645d1ba0a0c2c9d2c3
talasilasuresh <45663986+talasilasuresh@users.noreply.github.com>
2018-12-21 14:20:32 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// class -> PascalCasing
+// methods or variables -> camelCaseCharacters
+// constant -> FINAL
+// package -> package
+class HelloWorld{
+       public static void main(String [] args) {
+             System.out.println("Hello World");
+}
+}
